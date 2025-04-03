cf62e612dcbdb03190929c481827e8761f823941
Hannes Hertach <hanneshertach@gmail.com>
2025-04-03 21:24:03 +0200

    add tools

---
+++ tools/generator/generator.js
@@ -0,0 +1,25 @@
+/* eslint-disable import/no-unresolved */
+import 'https://da.live/nx/public/sl/components.js';
+import getStyle from 'https://da.live/nx/utils/styles.js';
+import {
+  LitElement, html,
+} from 'da-lit';
+
+const style = await getStyle(import.meta.url);
+
+class Generator extends LitElement {
+  async connectedCallback() {
+    super.connectedCallback();
+    this.shadowRoot.adoptedStyleSheets = [style];
+  }
+
+  render() {
+    return html`
+      <div class="main-container">
+        <h1>Generate Sites from Template</h1>
+      </div>
+    `;
+  }
+}
+
+customElements.define('da-generator', Generator);
